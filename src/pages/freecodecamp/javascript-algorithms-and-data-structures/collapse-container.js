c26a63e0f9c0ceb7d442c348b9c866ffd8ae9cd4
Fidel Sanchez <me@fidel.engineer>
2019-10-18 00:15:07 -0400

    working on 'freecodecamp/javascript-algorithms-and-data-structure'. Finished tweaking collapse-container component

--- src/pages/freecodecamp/javascript-algorithms-and-data-structures/collapse-container.js
+++ src/pages/freecodecamp/javascript-algorithms-and-data-structures/collapse-container.js
@@ -3,32 +3,6 @@ import {StaticQuery, graphql} from 'gatsby';
 import Img from 'gatsby-image';
 import styles from './collapse-container.module.css';
 
-const _DATA = graphql`
-  {
-    expandIcon: file(relativePath: {eq: "ic_add_black_48dp.png"}) {
-      childImageSharp {
-        fixed(width: 48) {
-          width
-          height
-          src
-          base64
-        }
-      }
-    }
-    checkWhiteIcon: file(relativePath: {eq: "ic_check_circle_white_48dp.png"}) {
-      childImageSharp {
-        fixed(width: 48) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-  }
-`;
-
-const ICON_STYLE = {
-  margin: '0px',
-  borderRadius: '50%',
-};
 class CollapseContainer extends React.Component {
   constructor(props) {
     // The ref might not be necessary, I guess I can just delete it?
@@ -90,31 +64,23 @@ class CollapseContainer extends React.Component {
       : {maxHeight: `${this.staticState.headerHeight}px`};
 
     return (
-      <StaticQuery
-        query={_DATA}
-        render={data => {
-          return (
-            <section
-              ref={this.componentRef}
-              className={styles.collapseContainer}
-              style={elementInlineStyle}>
-              <header
-                onClick={this.onHeaderClick}
-                className={styles.collapseHeader}>
-                <h3>{this.props.headerText}</h3>
-                <Img
-                  imgStyle={ICON_STYLE}
-                  className={styles.collapseHeaderImgWrapper}
-                  fixed={data.expandIcon.childImageSharp.fixed}
-                />
-              </header>
-              <article className={styles.collapseContent}>
-                {this.props.children}
-              </article>
-            </section>
-          );
-        }}
-      />
+      <section
+        ref={this.componentRef}
+        className={styles.collapseContainer}
+        style={elementInlineStyle}>
+        <header onClick={this.onHeaderClick} className={styles.collapseHeader}>
+          <h3>{this.props.headerText}</h3>
+          {this.state.isOpen ? (
+            <div
+              className={`${styles.headerImgContainer} ${styles.open}`}></div>
+          ) : (
+            <div className={styles.headerImgContainer}></div>
+          )}
+        </header>
+        <article className={styles.collapseContent}>
+          {this.props.children}
+        </article>
+      </section>
     );
   }
 }
